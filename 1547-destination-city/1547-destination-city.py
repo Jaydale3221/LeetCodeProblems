4e9d42a81ff2c48a384583e38373ddcbb8ef4a47
Jay Dale <63998734+Jaydale3221@users.noreply.github.com>
2023-06-30 19:50:32 -0400

    Time: 76 ms (29.36%), Space: 16.2 MB (99.93%) - LeetHub

--- 1547-destination-city/1547-destination-city.py
+++ 1547-destination-city/1547-destination-city.py
@@ -1,13 +1,21 @@
 class Solution:
     def destCity(self, paths: List[List[str]]) -> str:
-        city = collections.Counter()
+        # Create a counter to keep track of cities
+        city_counter = collections.Counter()
 
-        for right in paths:
-            city[right[0]] += 1
-            city[right[1]] += 0
+        # Iterate through each pair of cities in the paths
+        for pair in paths:
+            # Increment the count for the starting city
+            city_counter[pair[0]] += 1
+            city_counter[pair[1]] += 0
 
-        for pair in city:
-            if city[pair] == 0:
-                return pair
-        
+            # Initialize the count for the destination city (no need to increment)
+
+        # Iterate through the cities in city_counter
+        for city in city_counter:
+            # If the count for a city is 0, it means it is a destination city
+            if city_counter[city] == 0:
+                return city
+
+        # If no destination city is found, return an empty string
         return ""
