cf5cb5b5728b609e569f172579f76dd8eee1473d
Jay Dale <63998734+Jaydale3221@users.noreply.github.com>
2023-06-30 11:35:44 -0400

    Time: 51 ms (34.68%), Space: 16.4 MB (26.55%) - LeetHub

--- 0782-jewels-and-stones/0782-jewels-and-stones.py
+++ 0782-jewels-and-stones/0782-jewels-and-stones.py
@@ -1,4 +1,4 @@
 class Solution(object):
     def numJewelsInStones(self, J, S):
-        Jset = set(J)
-        return sum(s in Jset for s in S)
+        Jset = set(J)  # Create a set of unique characters from the string J (representing types of jewels)
+        return sum(s in Jset for s in S)  # Count the number of stones (characters in S) that are also present in the set of jewels (Jset)
