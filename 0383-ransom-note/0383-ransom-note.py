cf1c05c3131febc9e2e4ad55aff3e3a9fc97e7ab
Jay Dale <63998734+Jaydale3221@users.noreply.github.com>
2023-07-01 10:36:05 -0400

    Time: 63 ms (82.06%), Space: 16.5 MB (64.99%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,6 +1,7 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         counts = collections.Counter(magazine)  # Count the frequency of each character in the magazine
+      
         for i in range(len(ransomNote)):
             if counts[ransomNote[i]] <= 0:  # If the count of a character in the ransom note is zero or negative, it cannot be formed
                 return False
