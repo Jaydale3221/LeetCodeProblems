d5ab65088277f4591a4edf597897390dd2640d4a
Jay Dale <63998734+Jaydale3221@users.noreply.github.com>
2023-07-01 15:49:27 -0400

    Time: 1430 ms (13.63%), Space: 28.9 MB (96.79%) - LeetHub

--- 1813-maximum-erasure-value/1813-maximum-erasure-value.py
+++ 1813-maximum-erasure-value/1813-maximum-erasure-value.py
@@ -1,20 +1,19 @@
 class Solution:
     def maximumUniqueSubarray(self, nums: List[int]) -> int:
         counts = collections.Counter()
-        current = answer = left = 0
+        answer = left  = current = 0
 
         for right in range(len(nums)):
             current += nums[right]
 
-            counts[nums[right]] += 1
+            counts[nums[right]] += 1 
 
             while counts[nums[right]] > 1:
                 current -= nums[left]
                 counts[nums[left]] -= 1
-                left += 1
+
+                left+=1 
             
-            answer = max(current,answer)
+            answer = max(current, answer)
         
         return answer
-
-
