60614a5637f93557bd4e4be669bbe37790e91e78
Jay Dale <63998734+Jaydale3221@users.noreply.github.com>
2023-07-05 15:58:10 -0400

    Time: 66 ms (60.4%), Space: 17.1 MB (6.99%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,15 +1,21 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         stack = []
-        map = defaultdict(lambda: -1)
+        answer = [0 for i in range(len(nums1))]
+        counts = collections.Counter()
         
         for num in nums2:
-            while stack and num > stack[-1]:
-                map[stack.pop()] = num
+            while stack and stack[-1] < num:
+                counts[stack.pop()] = num
             stack.append(num)
+            
+        while stack:
+            counts[stack.pop()] = -1
         
-        res = []
-        for num in nums1:
-            res.append(map[num])
+        print(counts)
+        for i in range(len(nums1)):
+            answer[i] = counts[nums1[i]]
+        return answer
+            
+            
         
-        return res
