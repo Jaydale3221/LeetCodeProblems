5ab2e96a44614eede9fd8a9c777c803fbc79b2f2
Jay Dale <63998734+Jaydale3221@users.noreply.github.com>
2023-07-05 19:57:19 -0400

    Time: 64 ms (68.34%), Space: 16.6 MB (35.22%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -3,14 +3,15 @@ class Solution:
         stack = []
         answer = [0 for i in range(len(nums1))]
         counts = collections.Counter()
-        
+
         for num in nums2:
             while stack and stack[-1] < num:
                 counts[stack.pop()] = num
             stack.append(num)
-            
+
         while stack:
-            counts[stack.pop()] = -1
+            counts[stack.pop()] = -1 
+        
         
         for i in range(len(nums1)):
             answer[i] = counts[nums1[i]]
