af55f28770c5936903123ddc0db4c7f12f94c9fb
Jay Dale <63998734+Jaydale3221@users.noreply.github.com>
2023-07-06 19:57:29 -0400

    Time: 1256 ms (77.07%), Space: 29.4 MB (84.96%) - LeetHub

--- 1792-find-the-most-competitive-subsequence/1792-find-the-most-competitive-subsequence.py
+++ 1792-find-the-most-competitive-subsequence/1792-find-the-most-competitive-subsequence.py
@@ -3,14 +3,12 @@ class Solution:
         chances = len(nums) - k
         sequence = collections.deque()
 
-
         for num in nums:
             while sequence and sequence[-1] > num and chances != 0:
                 sequence.pop()
-                chances -= 1
+                chances -=1 
             sequence.append(num)
-            
-        # print(k)
+        
         return list(sequence)[:k]
 
-        
+ 
