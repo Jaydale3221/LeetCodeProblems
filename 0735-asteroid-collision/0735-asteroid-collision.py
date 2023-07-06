3ab46689a2af4cb5f772353f560c5059d65b6d3a
Jay Dale <63998734+Jaydale3221@users.noreply.github.com>
2023-07-06 15:58:26 -0400

    Time: 111 ms (61.55%), Space: 17.6 MB (61.83%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -1,25 +1,3 @@
-# class Solution:
-#     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-#         stack = []
-#         for asteroid in asteroids:
-#             flag = 1
-#             while stack and stack[-1] > 0 and asteroid < 0:
-#                 if abs(stack[-1]) > abs(asteroid):
-#                     flag = 0
-#                     break
-#                 elif abs(stack[-1]) < abs(asteroid):
-#                     stack.pop()
-#                 else:
-#                     stack.pop()
-#                     flag = 0
-#                     break
-#             if flag == 1:
-#                 stack.append(asteroid) 
-#         return stack
-
-
-
-
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
         result_stack = []  # Stores the remaining asteroids after collisions
