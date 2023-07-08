2c7003e522c4337f22cb2d9d65c2146b4c6f2659
Jay Dale <63998734+Jaydale3221@users.noreply.github.com>
2023-07-08 13:07:11 -0400

    Time: 51 ms (63.18%), Space: 16.2 MB (96.62%) - LeetHub

--- 1666-make-the-string-great/1666-make-the-string-great.py
+++ 1666-make-the-string-great/1666-make-the-string-great.py
@@ -1,17 +1,17 @@
 class Solution:
     def makeGood(self, s: str) -> str:
-        stack = []  # Initialize an empty stack to keep track of characters in the modified string
-
-        for i in range(len(s)):  # Iterate through each character in the input string
-            if i == 0:
-                stack.append(s[i])  # For the first character, directly append it to the stack
+        stack = []
+        for i in range(len(s)):
+            if i == 0 :
+                stack.append(s[i])
             elif stack and s[i] != stack[-1]:
-                # If stack is not empty and current character is different from the top character of the stack
                 if s[i].lower() == stack[-1].lower():
-                    stack.pop()  # Remove the top character from the stack if it forms a "bad" pair with the current character
+                    stack.pop()
                 else:
-                    stack.append(s[i])  # Append the current character to the stack
+                    stack.append(s[i])
             else:
-                stack.append(s[i])  # If current character is the same as the top character of the stack, append it to the stack
+                stack.append(s[i])
 
-        return "".join(stack) if stack else ""  # Join the characters in the stack to form the modified string, or return an empty string if the stack is empty
+        return "".join(stack) if stack else ""
+
+    
