6aa06d4a437f845101376d2ef979779cdc354837
Jay Dale <63998734+Jaydale3221@users.noreply.github.com>
2023-07-09 11:48:50 -0400

    Time: 374 ms (93.45%), Space: 21.7 MB (28.95%) - LeetHub

--- 0937-online-stock-span/0937-online-stock-span.py
+++ 0937-online-stock-span/0937-online-stock-span.py
@@ -2,16 +2,16 @@ class StockSpanner:
 
     def __init__(self):
         self.stack = []
+        
 
     def next(self, price: int) -> int:
-        count = 1
+        answer = 1 #counting self
 
         while self.stack and self.stack[-1][0] <= price:
-            count += self.stack.pop()[1]
-        self.stack.append([price,count])
-
-        return count
-
-# Your StockSpanner object will be instantiated and called as such:
-# obj = StockSpanner()
-# param_1 = obj.next(price)
+            # print(self.stack.pop()[1])
+            answer += self.stack.pop()[1]
+        self.stack.append([price,answer])
+        # print(price, answer)
+        return answer
+        
+        
