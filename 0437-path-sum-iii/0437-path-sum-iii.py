1bef7800bab203939677a06b6d02c415a8f5babb
Jay Dale <63998734+Jaydale3221@users.noreply.github.com>
2023-07-13 07:11:59 -0400

    Time: 801 ms (21.53%), Space: 18 MB (22.03%) - LeetHub

---
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: TreeNode, sum: int) -> int:
+        def helper(root, sum):
+            if not root: return 0
+
+            result = 0
+            if root.val == sum: result += 1
+
+            result += helper(root.left, sum - root.val)
+            result += helper(root.right, sum - root.val)
+            return result
+
+
+        if not root: return 0
+        return self.pathSum(root.left ,sum) + helper(root, sum) + self.pathSum(root.right, sum)
+
